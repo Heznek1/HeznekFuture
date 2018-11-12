d16fa7bb74fdf5aa54665c3f37625d29ef75e5ba
Adrian <sci.adrian@gmail.com>
2018-11-12 11:30:24 +0200

    Added WebApi Options; Autofac, NUnit, Moq, Autofac.Moq, JSON Return Type

--- HeznekLaatid.Services/HeznekLaatid.Services/App_Start/WebApiConfig.cs
+++ HeznekLaatid.Services/HeznekLaatid.Services/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace HeznekLaatid.Services
 {
@@ -9,7 +10,18 @@ namespace HeznekLaatid.Services
     {
         public static void Register(HttpConfiguration config)
         {
+            // CORS Attributes
+            var corsAttribute = new EnableCorsAttribute("*", "*", "*");
+
+            // Enable CORS
+            config.EnableCors(corsAttribute);
+
             // Web API configuration and services
+            config.Filters.Add(new AuthorizeAttribute());
+
+            // JSON Return type
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
