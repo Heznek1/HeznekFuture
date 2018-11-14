245b4323e16daa08f23803c03ad2cc370edcabf0
Adrian <sci.adrian@gmail.com>
2018-11-14 23:24:11 +0200

    Added and setup Autofac for dependency injection

---
+++ HeznekLaatid.Services/HeznekLaatid.Services/App_Start/Bootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace HeznekLaatid.Services
+{
+    public class Bootstrapper
+    {
+        public static void Run()
+        {
+            // Configure Autofac Dependency Resolver
+            DependencyResolver.Initialize(GlobalConfiguration.Configuration);
+        }
+    }
+}
---
+++ HeznekLaatid.Services/HeznekLaatid.Services/App_Start/DependencyResolver.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+
+namespace HeznekLaatid.Services
+{
+    public class DependencyResolver
+    {
+        private static IContainer container;
+
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterServices(new ContainerBuilder()));
+        }
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // Register needed components
+
+            // Set the dependency to Autofac
+            container = builder.Build();
+
+            return container;
+        }
+    }
+}
