f66eec0f494abeb72fe5b151e980465740a4848c
Adrian <sci.adrian@gmail.com>
2018-11-15 10:09:53 +0200

    Parsing data from token-service to hezneklaatid-service

---
+++ HeznekLaatid.Services/HeznekLaatid.Services/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Web.Http;
+using TokenService.Filters;
+
+namespace HeznekLaatid.Services.Controllers
+{
+    [JwtAuthentication]
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        [Route("api/values")]
+        public IHttpActionResult ReturnUser()
+        {
+            var currentUser = User as ClaimsPrincipal;
+            var usernameClaim = currentUser.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name).Value;
+            var roleClaim = currentUser.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
+            var instanceClaim = currentUser.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Actor)?.Value;
+
+            return Ok(new
+            {
+                usernameClaim,
+                roleClaim,
+                instanceClaim
+            });
+        }
+    }
+}
